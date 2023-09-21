511068be097f1a8ef7114267ef5111cc44c24962
Andy O'Brien <andygobrien@gmail.com>
2023-09-21 17:21:59 -0400

    basic form is functional

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,9 @@ use dioxus::prelude::*;
 use crate::steam_scheduler::InvisScheduler;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
-use dioxus::html::{br, form, input, label};
 use dioxus_desktop::{Config, WindowBuilder};
+use std::str::FromStr;
+use clokwerk::Interval::{Weekday};
 
 fn main() {
     dioxus_desktop::launch_cfg(
@@ -21,13 +22,14 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let scheduler = use_state(cx, || InvisScheduler {
+    use_shared_state_provider(cx, || InvisScheduler {
         interval: Interval::Weekday,
         start_time: NaiveTime::from_hms_opt(5, 0, 0).unwrap(),
         end_time: NaiveTime::from_hms_opt(17, 0, 0).unwrap(),
     });
+    let scheduler = use_shared_state::<InvisScheduler>(cx).unwrap();
     let is_start = use_state(cx, || true);
-    let stop= use_state(cx, || Arc::new(AtomicBool::default()));
+    let stop = use_state(cx, || Arc::new(AtomicBool::default()));
 
     cx.render(rsx! {
         div {
@@ -35,6 +37,15 @@ fn app(cx: Scope) -> Element {
             flex_direction: "column",
             align_content: "center",
             flex_wrap: "wrap",
+            p {
+                flex_direction: "column",
+                align_content: "center",
+                flex_wrap: "wrap",
+                text_align: "center",
+                margin_left: "50px",
+                margin_right: "50px",
+                "Select the interval and time range that you would like to show as invisible in steam"
+            }
             interval_input(cx, !is_start.get()),
             button {
                 width: "auto",
@@ -44,10 +55,10 @@ fn app(cx: Scope) -> Element {
                 onclick: move |_| {
                     if *is_start.get() {
                         stop.get().store(false, Ordering::SeqCst);
-                        scheduler.get().start_schedule(&stop);
+                        scheduler.read().start_schedule(&stop);
                         is_start.set(false);
                     } else {
-                        stop.get().store(true, Ordering::SeqCst);
+                        stop.store(true, Ordering::SeqCst);
                         is_start.set(true);
                     }
                 },
@@ -57,49 +68,84 @@ fn app(cx: Scope) -> Element {
                     "Stop"
                 },
             },
-            running(cx, is_start.get())
+            running(cx, is_start.get()),
         }
     })
 }
 
 fn interval_input(cx: Scope, disabled: bool) -> Element {
+    let scheduler = use_shared_state::<InvisScheduler>(cx).unwrap();
     cx.render(rsx! {
-        form {
+        div {
+            margin_bottom:"36px",
+            display: "flex",
+            flex_direction: "row",
+            align_content: "center",
+            align_self: "center",
+            flex_wrap: "wrap",
             div {
-                margin_bottom:"36px",
+                margin_right:"24px",
+                h3 {
+                    "Interval"
+                }
+                input { r#type:"radio", id:"weekday", name:"interval", disabled:disabled, checked:true,
+                    onchange: move |_| {
+                        let new_scheduler = InvisScheduler{
+                            interval: Weekday,
+                            start_time: scheduler.read().start_time.clone(),
+                            end_time: scheduler.read().start_time.clone(),
+                        };
+                        *scheduler.write() = new_scheduler;
+                    }
+                }
+                label { r#for:"weekday", "Weekday" } br {} br {}
+                input { r#type:"radio", id:"everyday", name:"interval", disabled: disabled,
+                    onchange: move |_| {
+                        let new_scheduler = InvisScheduler{
+                            interval: Interval::Days(1),
+                            start_time: scheduler.read().start_time.clone(),
+                            end_time: scheduler.read().start_time.clone(),
+                        };
+                        *scheduler.write() = new_scheduler;
+                    }
+                }
+                label { r#for:"everyday", "Everyday" }
+            }
+            div {
+                margin_left:"24px",
                 display: "flex",
-                flex_direction: "row",
+                flex_direction: "column",
                 align_content: "center",
                 flex_wrap: "wrap",
-                div {
-                    margin_right:"24px",
-                    h3 {
-                        "Interval"
-                    }
-                    input { r#type:"radio", id:"weekday", name:"interval", value:"weekday", disabled: disabled }
-                    label { r#for:"weekday", "Weekday" } br {} br {}
-                    input { r#type:"radio", id:"weekend", name:"interval", value:"weekend", disabled: disabled }
-                    label { r#for:"weekend", "Weekend" } br {} br {}
-                    input { r#type:"radio", id:"every-day", name:"interval", value:"every-day", disabled: disabled }
-                    label { r#for:"every-day", "Every day" }
+                h3 {
+                    "Range"
                 }
-                div {
-                    margin_left:"24px",
-                    display: "flex",
-                    flex_direction: "column",
-                    align_content: "center",
-                    flex_wrap: "wrap",
-                    h3 {
-                        "Range"
+                label { r#for:"start_time", "Start time" }
+                input { r#type:"time", id:"start_time", initial_value:"05:00:00", disabled: disabled,
+                    onchange: move |evt| {
+                        let new_scheduler = InvisScheduler {
+                            interval: scheduler.read().interval.clone(),
+                            start_time: NaiveTime::from_str(&evt.value).unwrap(),
+                            end_time: scheduler.read().end_time.clone(),
+                        };
+                        *scheduler.write() = new_scheduler;
                     }
-                    label { r#for:"start_time", "Start time" }
-                    input { r#type:"time", id:"start_time", disabled: disabled }
-                    br{}
-                    label { r#for:"end_time", "End time" }
-                    input { r#type:"time", id:"end_time", disabled: disabled }
+                }
+                br{}
+                label { r#for:"end_time", "End time" }
+                input { r#type:"time", id:"end_time", initial_value:"17:00:00", disabled: disabled,
+                    onchange: move |evt| {
+                        let new_scheduler = InvisScheduler {
+                            interval: scheduler.read().interval.clone(),
+                            start_time: scheduler.read().start_time.clone(),
+                            end_time: NaiveTime::from_str(&evt.value).unwrap(),
+                        };
+                        *scheduler.write() = new_scheduler;
+                    },
                 }
             }
         }
+
     })
 }
 
